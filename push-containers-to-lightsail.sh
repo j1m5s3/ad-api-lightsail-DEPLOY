44f6b2e0d326e4b72e58ba4930e552f47f2b5238
j1m5s3 <james.lynch.uml@gmail.com>
2023-01-01 16:12:55 -0600

    Initial commits for lightsail deployments

---
+++ push-containers-to-lightsail.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $1 == "nginx" ]; then
+    echo "Pushing NGINX container to lightsail...";
+    aws lightsail push-container-image --service-name ad-api-container-service-1 --label ad-api-nginx --image ad-api-nginx/v1;
+fi
+
+if [ $1 == "be" ]; then
+    echo "Pushing BE container to lightsail...";
+    aws lightsail push-container-image --service-name ad-api-container-service-1 --label ad-api-be --image ad-api-be/v1;
+fi
+
+if [ $1 == "db" ]; then
+    echo "Pushing DB container to lightsail...";
+    aws lightsail push-container-image --service-name ad-api-container-service-1 --label ad-api-db --image ad-api-db/v1;
+fi
+
+if [ $1 == "all" ]; then
+    echo "Pushing all containers to lightsail...";
+    aws lightsail push-container-image --region us-east-1 --service-name ad-api-container-service-1 --label ad-api-nginx --image ad-api-nginx/v1;
+    aws lightsail push-container-image --region us-east-1 --service-name ad-api-container-service-1 --label ad-api-be --image ad-api-be/v1;
+    aws lightsail push-container-image --region us-east-1 --service-name ad-api-container-service-1 --label ad-api-db --image ad-api-db/v1;
+fi
+
+echo "Done..."
